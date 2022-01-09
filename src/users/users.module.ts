0cc3831dc16d238e3bc9fcb0da27943000d34e2f
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 16:20:43 +1100

    Rename user to users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user.entity';
+import { UserEntity } from './users.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
