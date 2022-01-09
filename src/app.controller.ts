94d3aa6290523e62f00445e15f7cddc1147efb51
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 16:18:33 +1100

    Add Auth module controller Remove app level controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,8 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
+import { Controller, Request, UseGuards, Get } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
