74fa25a99a9232da9e9caf3ac1b04edb426e066b
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 16:20:31 +1100

    Rename auth mock service method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,7 +18,7 @@ export class UsersService {
     },
   ];
 
-  async findOne(username: string): Promise<User | undefined> {
+  async findOneAuthMock(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
   }
 }
