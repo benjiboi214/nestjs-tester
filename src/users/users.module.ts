d9f373b0899d9efd11b536c9fda4b4efad27ebe5
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 15:37:25 +1100

    Add users controller using the CLI

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
