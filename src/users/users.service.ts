c541c94e373cf7901352c673b877e03aaff170c1
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 16:21:00 +1100

    Add repository methods to service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './users.entity';
 
 // This should be a real class/interface representing a user entity
 export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+  ) {}
+
   private readonly users = [
     {
       userId: 1,
@@ -17,6 +25,18 @@ export class UsersService {
       password: 'guess',
     },
   ];
+
+  findAll(): Promise<UserEntity[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<UserEntity> {
+    return this.usersRepository.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
 
   async findOneAuthMock(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
