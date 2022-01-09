966edf12b256ade3fece18422dbe92506c64655c
Ben Elliot <benjiboi214@gmail.com>
2022-01-09 15:35:34 +1100

    Add typeorm configuration for local testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UserEntity } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 1234,
+      username: 'nest_db_test',
+      password: 'A^XWX4mX2y2Nbmn29ZAd^%qC34wAJ3',
+      database: 'nest_db_test',
+      entities: [UserEntity],
+      synchronize: true, // Do not use this in production
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
